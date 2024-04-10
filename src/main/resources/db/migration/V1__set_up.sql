21f4582d52ae1f3acaef830b4f9c4f8a0416fb45
Sebastian Blanco <sebastianblancogonz@gmail.com>
2024-04-10 14:21:16 +0200

    Revert "chore: Update properties and flyway migration to postgresql"
    This reverts commit 13c1b1135a5966d2390809c8d8869f202348824d.

--- src/main/resources/db/migration/V1__set_up.sql
+++ src/main/resources/db/migration/V1__set_up.sql
@@ -1,8 +1,6 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-DROP TABLE users;
 CREATE TABLE users (
-    id UUID DEFAULT Uuid_generate_v4 (),
-    username VARCHAR(50),
+    id varchar(36),
+    username varchar(50),
     email varchar(120),
     password varchar(320)
 );
