16ba2b52284b1afdf2a83a6996b448390440b895
Sebastian Blanco <sebastianblancogonz@gmail.com>
2024-04-10 08:12:01 +0200

    test: Fix controller test

--- src/main/kotlin/com/crosstime/backend/controller/UsersController.kt
+++ src/main/kotlin/com/crosstime/backend/controller/UsersController.kt
@@ -1,13 +1,13 @@
 package com.crosstime.backend.controller
 
 import com.crosstime.backend.request.UserRequest
+import com.crosstime.backend.response.UserResponse
 import com.crosstime.backend.service.UsersService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,8 +16,9 @@ class UsersController(
 ) {
 
     @PostMapping
-    fun createUser(@RequestBody request: UserRequest): ResponseEntity<UUID> {
-        return ResponseEntity.ok(usersService.createUser(request.email, request.username));
+    fun createUser(@RequestBody request: UserRequest): ResponseEntity<UserResponse> {
+        val userId = usersService.createUser(request.email, request.username)
+        return ResponseEntity.ok(UserResponse(userId));
     }
 
 }
--- src/test/groovy/com/crosstime/backend/controller/UsersControllerTest.groovy
+++ src/test/groovy/com/crosstime/backend/controller/UsersControllerTest.groovy
@@ -1,8 +1,11 @@
 package com.crosstime.backend.controller
 
+import com.crosstime.backend.request.UserRequest
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -19,11 +22,17 @@ class UsersControllerTest extends Specification {
     @Autowired
     private MockMvc mockMvc
 
+    @Autowired
+    private ObjectMapper objectMapper
+
     def "should return users"() {
+        given: "A create user request"
+        def userRequest = new UserRequest("username", "email")
         expect: "Status is 200 and the response is the hardcoded user"
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(userRequest)))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath('$.username').value("Username"))
     }
 
 }
