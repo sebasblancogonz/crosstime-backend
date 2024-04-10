281d90f01d9782c14b6d7f5186856aeb9000e429
Sebastian Blanco <sebastianblancogonz@gmail.com>
2024-04-10 07:58:17 +0200

    fix: Add request mapping annotation to the controller

--- src/main/kotlin/com/crosstime/backend/controller/UsersController.kt
+++ src/main/kotlin/com/crosstime/backend/controller/UsersController.kt
@@ -1,15 +1,16 @@
 package com.crosstime.backend.controller
 
-import com.crosstime.backend.model.User
 import com.crosstime.backend.request.UserRequest
 import com.crosstime.backend.service.UsersService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
-@RestController(value = "/api/users")
+@RestController
+@RequestMapping("/api/users")
 class UsersController(
     val usersService: UsersService
 ) {
